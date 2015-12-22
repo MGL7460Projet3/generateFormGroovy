1b72b866b0eda9dedbe40402f3c85e349994bef8
lamrous <lamrous.lyes@gmail.com>
2015-12-21 23:40:32 -0500

    completer le dsl

--- src/dsl/MyForm.groovy
+++ src/dsl/MyForm.groovy
@@ -6,14 +6,33 @@ public class MyForm extends FormBuilder {
 
     void configureForm() { 
         form {
-            input { e ->
-                e.type = "text"
-                e.value = "input1"
+            label { e ->
+                e.text = "label1"
             }
             input { e ->
-                e.type = "text"
-                e.value = "input2"
+                e.name = "input1"
             }
+            input { e ->
+                e.text = "label2"
+            }
+            input { e ->
+                e.name = "input2"
+            }
+            checkbox { e ->
+                e.name = "checkbox1"
+                e.value = "checkbox1"
+                e.checked = "checked"
+                e.text = "checkbox1"
+            }
+            checkbox { e ->
+                e.name = "checkbox1"
+                e.value = "checkbox1"
+                e.checked = ""
+                e.text = "checkbox1"
+            }
+            button { e ->
+                e.value = "submit"
+            }		
         }
     }
 
